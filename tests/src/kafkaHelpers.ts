00b3980ea3d47bd63f859dab58ae67b951ebd03d
m-pozyczka <m.pozyczka@samsung.com>
2026-09-20 20:55:08 +0200

    Delete fromBeginning: true

--- tests/src/kafkaHelpers.ts
+++ tests/src/kafkaHelpers.ts
@@ -135,7 +135,7 @@ export async function waitForMessage(
 ): Promise<any | null> {
   const consumer = newConsumer();
   await consumer.connect();
-  await consumer.subscribe({ topic, fromBeginning: true });
+  await consumer.subscribe({ topic });
 
   return new Promise((resolve, reject) => {
     let settled = false;
@@ -190,7 +190,7 @@ export async function collectMessages(
 ): Promise<any[]> {
   const consumer = newConsumer();
   await consumer.connect();
-  await consumer.subscribe({ topic, fromBeginning: true });
+  await consumer.subscribe({ topic });
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const found: any[] = [];
